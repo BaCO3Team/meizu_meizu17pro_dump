272762d3b580959803c9a2423c166478f50141a5
root <root@fv-az670-383.vvj1krob40nuzn3jnzu5queufe.phxx.internal.cloudapp.net>
2022-12-27 15:46:20 +0000

    Add extras for qssi-user 11 QKQ1.200127.002 1645164086 release-keys
    Signed-off-by: root <root@fv-az670-383.vvj1krob40nuzn3jnzu5queufe.phxx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 11 QKQ1.200127.002 1645164086 release-keys
+- Manufacturer: meizu
+- Platform: 
+- Codename: meizu17Pro
+- Brand: meizu
+- Flavor: qssi-user
+- Release Version: 11
+- Id: QKQ1.200127.002
+- Incremental: 1645164086
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: undefined
+- Fingerprint: meizu/meizu_17Pro_N_CN/meizu17Pro:11/QKQ1.200127.002/1645164086:user/release-keys
+- OTA version: 
+- Branch: qssi-user-11-QKQ1.200127.002-1645164086-release-keys
+- Repo: meizu_meizu17pro_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cat system/system/priv-app/VoiceAssistant/VoiceAssistant.apk.* 2>/dev/null >> system/system/priv-app/VoiceAssistant/VoiceAssistant.apk
+rm -f system/system/priv-app/VoiceAssistant/VoiceAssistant.apk.* 2>/dev/null
+cat system/system/priv-app/Video/Video.apk.* 2>/dev/null >> system/system/priv-app/Video/Video.apk
+rm -f system/system/priv-app/Video/Video.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/DirectService/DirectService.apk.* 2>/dev/null >> system/system/priv-app/DirectService/DirectService.apk
+rm -f system/system/priv-app/DirectService/DirectService.apk.* 2>/dev/null
+cat system/system/priv-app/FlymeGallery/FlymeGallery.apk.* 2>/dev/null >> system/system/priv-app/FlymeGallery/FlymeGallery.apk
+rm -f system/system/priv-app/FlymeGallery/FlymeGallery.apk.* 2>/dev/null
+cat system/system/MzApp/ARruler/ARruler.apk.* 2>/dev/null >> system/system/MzApp/ARruler/ARruler.apk
+rm -f system/system/MzApp/ARruler/ARruler.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/system_ext/app/SnapdragonSVA/SnapdragonSVA.apk.* 2>/dev/null >> system/system/system_ext/app/SnapdragonSVA/SnapdragonSVA.apk
+rm -f system/system/system_ext/app/SnapdragonSVA/SnapdragonSVA.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null >> system/system/system_ext/priv-app/SystemUI/SystemUI.apk
+rm -f system/system/system_ext/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null
